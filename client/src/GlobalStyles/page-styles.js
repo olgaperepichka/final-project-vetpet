0137c13f5f694a448e03ab7462fa454a04b088c7
olgaperepichka <olga_perepichka@yahoo.ca>
2022-05-08 13:27:42 -0400

    Homepage with API data

--- client/src/GlobalStyles/page-styles.js
+++ client/src/GlobalStyles/page-styles.js
@@ -1,18 +1,24 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   margin-top: 80px;
   text-align: center;
-`
+`;
 const Title = styled.h1`
   color: var(--color-orange);
   text-transform: uppercase;
   font-size: 52px;
-`
+`;
 
-export {Wrapper};
-export {Title};
+const Subtitle = styled.h2`
+  color: var(--color-mediumgray);
+  line-height: 1.3;
+  font-size: 36px;
+  font-weight: 300;
+  text-shadow: 0 1px 0px black;
+  padding: 20px;
+`;
 
-
-
+export { Wrapper };
+export { Title };
+export { Subtitle };
